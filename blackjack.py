2001f3e033638f551e0e83fb510e83f140c8dac5
MrJensenk <Noname2er3@yandex.ru>
2024-05-03 14:33:08 +0300

    Добавлено отображение 3д графика изменения значений карт у дилера и игрока а на основе первой задуманной стратегии(только на основе состояний, без Q-values)

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,13 @@
+import shutil
 import typing as tp
+from pathlib import Path
+
 import cv2
 import gymnasium as gym
 import imageio
 import numpy as np
 import plotly.graph_objects as go
+import plotly.io as pio
 from plotly.subplots import make_subplots
 from tqdm import tqdm
 
@@ -47,7 +51,7 @@ def take_action_from_policy_grid(dealer_card: int, player_scores: int, player_ha
     return action
 
 
-def estimate_policy(n_experiments: int = 100_000, out_file_name=None, render_every_n_episode: int = 1_000) -> float:
+def estimate_policy(n_experiments: int = 100000, out_file_name=None, render_every_n_episode: int = 1000) -> float:
     global current_avg_reward
     env = gym.make("Blackjack-v1", sab=True, render_mode='rgb_array' if out_file_name is not None else None)
 
@@ -90,7 +94,6 @@ def estimate_policy(n_experiments: int = 100_000, out_file_name=None, render_eve
 default_player_reward = estimate_policy(out_file_name="default_policy_game.mp4")
 print(default_player_reward)
 
-
 camera = dict(
     up=dict(x=0, y=0, z=1),
     center=dict(x=0, y=0, z=0),
@@ -98,19 +101,96 @@ camera = dict(
 )
 
 
-def draw_value(title: str, values: tp.Dict[str, np.ndarray]) -> go.Figure:
-    fig = make_subplots(rows=1, col=2, shared_xaxes=False, specs=[[{'type': 'surface'}, {'type': 'surface'}]],
+def draw_value_function(title: str, values: tp.Dict[str, np.ndarray]) -> go.Figure:
+    fig = make_subplots(rows=1, cols=2,
+                        shared_xaxes=False,
+                        specs=[[{'type': 'surface'}, {'type': 'surface'}]],
                         subplot_titles=["No ace", "Ace"])
+
     fig.add_trace(go.Surface(y=card_names, x=player_scores_axis,
-                             z=values['no ace'], colorScale='YlGnBu'), col=1, row=1)
+                             z=values['no_ace'], colorscale='YlGnBu'), col=1, row=1)
     fig.layout.scene1.camera = camera
     fig.layout.scene1.xaxis.nticks = 9
     fig.layout.scene1.yaxis.nticks = 10
-    fig.add_trace(go.Surface(y=card_names, x=player_scores_axis,
-                             z=values['ace'], colorscale='YlGnBu'), col=2, row=1)
+    fig.add_trace(go.Surface(y=card_names, x=player_scores_axis, z=values['ace'], colorscale='YlGnBu'), col=2, row=1)
+    fig.layout.scene2.camera = camera
     fig.layout.scene2.xaxis.nticks = 10
     fig.layout.scene2.yaxis.nticks = 10
     fig.update_layout(scene_camera=camera, title=title,
                       margin=dict(r=25, l=25, b=10, t=80),
                       width=1000,
                       showlegend=False)
+    fig.update_scenes(xaxis_title_text='Player',
+                      yaxis_title_text='Dealer',
+                      zaxis_title_text='Reward')
+    return fig
+
+
+def estimate_value_function(num_episodes: int = 300_000, frame_step: int = 3_000):
+    env = gym.make("Blackjack-v1", sab=True)
+
+    temp_dir = Path('tmp')
+    if temp_dir.exists():
+        shutil.rmtree(temp_dir)
+    temp_dir.mkdir()
+
+    value_grid_count: tp.Dict[str, np.ndarray] = {
+        'no_ace': np.zeros((10, 10), dtype=int),
+        'ace': np.zeros((10, 10), dtype=int)
+    }
+    value_grid: tp.Dict[str, np.ndarray] = {
+        'no_ace': np.zeros((10, 10), dtype=float),
+        'ace': np.zeros((10, 10), dtype=float)
+    }
+
+    value_grid['no_ace'][:, -1] = 1
+    value_grid['ace'][:, -1] = 1
+
+    image_paths = []
+
+    for ep_idx in tqdm(range(num_episodes + frame_step)):
+
+        obs, info = env.reset()
+        done = False
+
+        while not done:
+            player_scores, dealer_card, player_has_ace = obs
+            action = take_action_from_policy_grid(dealer_card, player_scores, player_has_ace)
+
+            next_obs, reward, terminated, truncated, info = env.step(action)
+
+            done = terminated or truncated
+
+            if player_scores >= player_scores_offset:
+
+                v_g_count = value_grid_count['ace' if player_has_ace else 'no_ace']
+                v_g = value_grid['ace' if player_has_ace else 'no_ace']
+
+                cell_idx = (dealer_card - dealer_card_offset, player_scores - player_scores_offset)
+
+                N = v_g_count[cell_idx] + 1
+                v_g_count[cell_idx] = N
+                if done:
+                    td_target = reward
+                else:
+                    next_player_scores, next_dealer_card, next_player_has_ace = next_obs
+                    next_v_g = value_grid['ace' if next_player_has_ace else 'no_ace']
+
+                    next_cell_idx = (next_dealer_card - dealer_card_offset, next_player_scores - player_scores_offset)
+                    td_target = reward + next_v_g[next_cell_idx]
+                v_g[cell_idx] += (td_target - v_g[cell_idx]) / N
+
+            obs = next_obs
+
+        if ep_idx % frame_step == 0:
+            ep_name = str(ep_idx) if ep_idx > 0 else '1'
+            fig = draw_value_function(f'Value function on step: {ep_name}', value_grid)
+
+            image_path = temp_dir / f'{ep_name}.png'
+            pio.write_image(fig, image_path, format='png')
+            image_paths.append(imageio.v2.imread(image_path))
+
+    imageio.v2.mimsave('blackjack_default_value_function.mp4', image_paths, fps=20)
+
+
+estimate_value_function()
