7862069700a3b08b2dda21b57a78b4df43b763fc
MrJensenk <Noname2er3@yandex.ru>
2024-06-05 18:51:04 +0300

    Добавлена вторая стратегия по оптимальному выигрышу, основанная на q-values, которая является более оптимизированной(высчитывается среднее значение выигрыша). Помимо этого, аналогично добавлено отображение 3D-графика по зависимостям значений карт дилера и агента, основанное на новой стратегии

--- blackjack.py
+++ blackjack.py
@@ -1,13 +1,16 @@
+import math
 import shutil
 import typing as tp
+from collections import deque
 from pathlib import Path
-
+import random
 import cv2
 import gymnasium as gym
 import imageio
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+import plotly.express as px
 from plotly.subplots import make_subplots
 from tqdm import tqdm
 
@@ -194,3 +197,112 @@ def estimate_value_function(num_episodes: int = 300_000, frame_step: int = 3_000
 
 
 estimate_value_function()
+
+
+q_grid: tp.Dict[str, np.ndarray] = {
+    'no_ace': np.zeros((10, 10, 2), dtype=float),
+    'ace': np.zeros((10, 10, 2), dtype=float)
+}
+
+
+def estimate_q_function(num_episodes: int = 10_000_000, frame_step: int = 100_000, plot_step: int = 10_000):
+    global q_grid
+
+    last_rewards = deque(maxlen=1000)
+    last_td_errors = deque(maxlen=1000)
+
+    mean_rewards = []
+    mean_td_errors = []
+
+    env = gym.make("Blackjack-v1", sab=True)
+
+    temp_dir = Path('tmp')
+    if temp_dir.exists():
+        shutil.rmtree(temp_dir)
+    temp_dir.mkdir()
+
+    eps_from = 1.0
+    eps_to = 1e-3
+    n_epochs_of_decays = math.ceil(num_episodes * 0.5)
+
+    q_grid_count: tp.Dict[str, np.ndarray] = {
+        'no_ace': np.zeros((10, 10, 2), dtype=int),
+        'ace': np.zeros((10, 10, 2), dtype=int)
+    }
+
+    frames = []
+
+    for ep_idx in tqdm(range(num_episodes + frame_step)):
+        if ep_idx > n_epochs_of_decays:
+            eps_greedy_coeff = 0.0
+        else:
+            step_coeff = min(max(ep_idx / n_epochs_of_decays, 0.0), 1.0)
+            eps_greedy_coeff = eps_from * math.exp(math.log(eps_to / eps_from) * step_coeff)
+            # eps_greedy_coeff = eps_from + (eps_to - eps_from) * step_coeff
+
+        obs, info = env.reset()
+        done = False
+        ep_reward = 0
+
+        while not done:
+            player_scores, dealer_card, player_has_ace = obs
+            q = q_grid['ace' if player_has_ace else 'no_ace']
+            q_count = q_grid_count['ace' if player_has_ace else 'no_ace']
+            if player_scores >= player_scores_offset:
+                cell_idx = (dealer_card - dealer_card_offset, player_scores - player_scores_offset)
+                if random.uniform(0, 1) < eps_greedy_coeff:
+                    action = np.random.choice([0, 1])
+                else:
+                    action = q[cell_idx].argmax()
+            else:
+                action = 1
+
+            next_obs, reward, terminated, truncated, info = env.step(action)
+            ep_reward += reward
+
+            done = terminated or truncated
+
+            if player_scores >= player_scores_offset:
+                if terminated:
+                    td_target = reward
+                else:
+                    next_player_scores, next_dealer_card, next_player_has_ace = next_obs
+                    next_cell_idx = (next_dealer_card - dealer_card_offset, next_player_scores - player_scores_offset)
+                    next_q = q_grid['ace' if next_player_has_ace else 'no_ace']
+
+                    td_target = reward + next_q[next_cell_idx].max()
+                td_error = td_target - q[cell_idx][action]
+                q_count[cell_idx][action] += 1
+                q[cell_idx][action] += td_error / q_count[cell_idx][action]
+                last_td_errors.append(abs(td_error))
+
+            obs = next_obs
+
+        last_rewards.append(ep_reward)
+
+        mean_reward = sum(last_rewards) / len(last_rewards)
+        if len(last_td_errors) > 0 and math.ceil(ep_idx / plot_step) >= len(mean_rewards):
+            mean_rewards.append(mean_reward)
+            mean_td_error = sum(last_td_errors) / len(last_td_errors)
+            mean_td_errors.append(mean_td_error)
+
+        if ep_idx % frame_step == 0:
+            ep_name = str(ep_idx) if ep_idx > 0 else '1'
+
+            values = {
+                'no_ace': q_grid['no_ace'].max(axis=2),
+                'ace': q_grid['ace'].max(axis=2)
+            }
+            fig = draw_value_function(f'Value-function from Q-function on step: {ep_name}', values)
+
+            image_path = temp_dir / f'{ep_name}.png'
+            fig.write_image(image_path)
+            frames.append(imageio.imread(image_path))
+
+    imageio.mimsave('blackjack_optimal_value_function.mp4', frames)
+
+    px.line(y=mean_rewards, title='Mean reward').show()
+    px.line(y=last_td_errors, title='Mean td errors').show()
+
+
+estimate_q_function()
